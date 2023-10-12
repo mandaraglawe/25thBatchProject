2bbb492a86c25187c9eed89d891f596956e20121
mandaraglawe <mandaraglawe2000@gmail.com>
2023-10-12 22:40:46 +0530

    msg-my third commit

---
+++ src/test/java/finalProject/SampleClass.java
@@ -0,0 +1,13 @@
+package finalProject;
+
+import org.testng.annotations.Test;
+
+public class SampleClass 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("Lets see what happens in github when we add a new class");
+	}
+
+}
